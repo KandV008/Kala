5cd20017ba1d28d2f9e67990a738e1ee4e2ee152
Ikár Mar de Liz <117300490+KandV008@users.noreply.github.com>
2024-02-29 20:05:56 +0100

    feat: Add SmallTextInputComponent.kt
    With configuration file for Input Components

---
+++ app/src/main/java/com/example/kala/configuration/ConfigurationInputComponents.kt
@@ -0,0 +1,52 @@
+package com.example.kala.configuration
+
+import com.example.kala.R
+
+/*
+    SmallTextInputComponent
+ */
+
+enum class SmallTextInputConfiguration(
+    private val layer: String,
+    private val placeholder: String,
+    private val svgFile: Int,
+){
+    USERNAME(
+        "Username",
+        "exampleUser",
+        R.drawable.ic_account,
+    ),
+    EMAIL(
+        "Email",
+        "youremail@email.com",
+        R.drawable.ic_email,
+    ),
+    PASSWORD(
+        "Password",
+        "******",
+        R.drawable.ic_password,
+    ),
+    NEW_PASSWORD(
+        "New password",
+        "******",
+        R.drawable.ic_password,
+    ),
+    REPEAT_PASSWORD(
+        "Repeat password",
+        "******",
+        R.drawable.ic_password,
+    );
+
+    fun getLayer(): String{
+        return layer
+    }
+
+    fun getPlaceholder(): String{
+        return placeholder
+    }
+
+    fun getSVGFile(): Int{
+        return svgFile
+    }
+
+}
